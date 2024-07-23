f52e11937698db8284af8e10c8e574b2a36ff02e
Asko Nõmm <asko@nth.ee>
2024-07-24 00:11:45 +0300

    No need for tailwind or npm anymore.

--- src/Blocks/ExampleAcfBlock/Block.php
+++ src/Blocks/ExampleAcfBlock/Block.php
@@ -7,70 +7,70 @@ use Wptf\Core\View;
 
 class Block extends AcfBaseBlock
 {
-    /**
-     * Block title
-     *
-     * @var string $title
-     */
-    public string $title = 'Example ACF Block';
+	/**
+	 * Block title
+	 *
+	 * @var string $title
+	 */
+	public string $title = 'Example ACF Block';
 
-    /**
-     * Block description
-     *
-     * @var string $description
-     */
-    public string $description = 'An example block.';
+	/**
+	 * Block description
+	 *
+	 * @var string $description
+	 */
+	public string $description = 'An example block.';
 
-    /**
-     * Block icon
-     *
-     * @var string $icon
-     */
-    public string $icon = 'admin-comments';
+	/**
+	 * Block icon
+	 *
+	 * @var string $icon
+	 */
+	public string $icon = 'admin-comments';
 
-    /**
-     * Block category
-     *
-     * @var string $category
-     */
-    public string $category = 'widgets';
+	/**
+	 * Block category
+	 *
+	 * @var string $category
+	 */
+	public string $category = 'widgets';
 
-    /**
-     * Block keywords
-     *
-     * @var array $keywords
-     */
-    public array $keywords = ['example'];
+	/**
+	 * Block keywords
+	 *
+	 * @var array $keywords
+	 */
+	public array $keywords = ['example'];
 
-    /**
-     * Block supports
-     *
-     * @var array $supports
-     */
-    public array $supports = [
-        'anchor' => true
-    ];
+	/**
+	 * Block supports
+	 *
+	 * @var array $supports
+	 */
+	public array $supports = [
+		'anchor' => true
+	];
 
-    /**
-     * @param array $block
-     * @param string $content
-     * @param bool $is_preview
-     * @param int $post_id
-     * @return string
-     * @throws \Exception
-     */
-    public function render(array $block, string $content, bool $is_preview, int $post_id): string
-    {
-        return (new View(__DIR__))->make('block', [
-            'content' => $content
-        ]);
-    }
+	/**
+	 * @param array $block
+	 * @param string $content
+	 * @param bool $is_preview
+	 * @param int $post_id
+	 * @return string
+	 * @throws \Exception
+	 */
+	public function render(array $block, string $content, bool $is_preview, int $post_id): string
+	{
+		return (new View(__DIR__))->make('block', [
+			'content' => $content
+		]);
+	}
 
-    /**
-     * @return void
-     */
-    public function assets(): void
-    {
-        // enqueue scripts and styles (no need to enqueue tailwind, that comes automatically)
-    }
+	/**
+	 * @return void
+	 */
+	public function assets(): void
+	{
+		// enqueue scripts and styles 
+	}
 }
